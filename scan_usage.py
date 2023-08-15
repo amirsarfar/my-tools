7c59e125fd7b24bba7600bdadc51ba9b7cd72892
Amir Sarfarazi <amirsarfar@gmail.com>
2023-08-15 14:31:20 +0330

    clean up and refactor

--- scan_usage.py
+++ scan_usage.py
@@ -7,9 +7,9 @@ from tqdm import tqdm
 
 parser=argparse.ArgumentParser()
 
-parser.add_argument("--rel", action="store_true", help="Relative target file")
-parser.add_argument("-t", help="Target file for tokens to search")
-parser.add_argument("-r", help="Directory path to search")
+parser.add_argument("--rel", action="store_true", help="Relative target directory")
+parser.add_argument("-t", help="Target directory for tokens to search")
+parser.add_argument("-r", help="Root directory path to search")
 parser.add_argument("-e", help="File extensions to search separated by space")
 
 args=parser.parse_args()
@@ -19,28 +19,30 @@ file_extensions = (args.e or "").split(" ")
 target_file = args.t if not args.rel else  rootdir + args.t
 
 tokens = []
-tt = {}
+tokens_dict = {}
 def scan(root=rootdir):
   for folder, dirs, files in os.walk(root):
     for file in files:
       yield os.path.join(folder, file)
 
+def get_js_parsed_ast(file_content):
+  resp = requests.post("http://127.0.0.1:3000/parse", data=file_content.encode("utf-8"), headers={"content-type":"text/plain"})
+  return json.loads(resp.text)["ast"]["body"]
+
 for fullpath in scan(target_file):
   if any(fullpath.endswith(x) for x in [".js"]):
     with open(fullpath, 'r') as f:
-      cc = f.read()
-      resp = requests.post("http://127.0.0.1:3000/parse", data=cc.encode("utf-8"), headers={"content-type":"text/plain"})
-      js = json.loads(resp.text)["ast"]["body"]
-      for ide in js:
-        if ide["type"] == "ExportNamedDeclaration":
-          if ide["declaration"]["type"] == "FunctionDeclaration":
-            tokens.append(ide["declaration"]["id"]["name"])
-            tt[ide["declaration"]["id"]["name"]] = fullpath + ":" + str(ide["declaration"]["loc"]["start"]["line"])
-            print(ide["declaration"]["type"], ide["declaration"]["id"]["name"], ide["declaration"]["loc"])
-          if ide["declaration"]["type"] == "VariableDeclaration":
-            tokens.append(ide["declaration"]["declarations"][0]["id"]["name"])
-            tt[ide["declaration"]["declarations"][0]["id"]["name"]] = fullpath + ":" + str(ide["declaration"]["loc"]["start"]["line"])
-            print(ide["declaration"]["type"], ide["declaration"]["declarations"][0]["id"]["name"], ide["declaration"]["loc"])
+      identifiers = get_js_parsed_ast(f.read())
+      for identifier in identifiers:
+        if identifier["type"] == "ExportNamedDeclaration":
+          main_dec = identifier["declaration"]
+          if main_dec["type"] == "FunctionDeclaration":
+            tokens.append(main_dec["id"]["name"])
+            tokens_dict[main_dec["id"]["name"]] = fullpath + ":" + str(main_dec["loc"]["start"]["line"])
+          if main_dec["type"] == "VariableDeclaration":
+            for declaration in main_dec["declarations"]:
+              tokens.append(declaration["id"]["name"])
+              tokens_dict[declaration["id"]["name"]] = fullpath + ":" + str(main_dec["loc"]["start"]["line"])
 
 
 filecounter = 0
@@ -48,16 +50,14 @@ for filepath in scan():
   filecounter += 1
 
 used_tokens = set()
-log_filename = "usage_scan"+ str(int(time.time())) +".log"
+log_filename = "usage_scan_"+ str(int(time.time())) +".log"
 for fullpath in tqdm(scan(), total=filecounter, unit="files"):
   if any(fullpath.endswith(x) for x in file_extensions):
     with open(fullpath, 'r') as f:
-      cc = f.read()
-      resp = requests.post("http://127.0.0.1:3000/parse", data=cc.encode("utf-8"), headers={"content-type":"text/plain"})
-      js = json.loads(resp.text)["ast"]["body"]
-      for ide in js:
-        if ide["type"] == "ImportDeclaration":
-          for specifier in ide["specifiers"]:
+      identifiers = get_js_parsed_ast(f.read())
+      for identifier in identifiers:
+        if identifier["type"] == "ImportDeclaration":
+          for specifier in identifier["specifiers"]:
             if specifier["type"] == "ImportSpecifier" and specifier["imported"]["name"] in tokens:
               used_tokens.add(specifier["imported"]["name"])
               with open(log_filename, "a") as myfile:
@@ -66,6 +66,8 @@ for fullpath in tqdm(scan(), total=filecounter, unit="files"):
 def filter_func(token):
   return token not in used_tokens
 def map_func(token):
-  return token + " " + tt[token]
+  return token + " " + tokens_dict[token]
 unused_tokens = list(map(map_func, filter(filter_func, tokens)))
-print("Unused tokens: ", "\n".join(unused_tokens))
+print("Unused tokens: \n", "\n".join(unused_tokens))
+
+print("\n\nLog file: ", log_filename)
